3762adc69eeaf6c0e2705b6928102c432fa03101
Michael <madetunji1376@gmail.com>
2022-11-21 16:03:28 +0100

    rspec installed for testing models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  user = User.new(
+    name: 'Buff Daddy'
+  )
+
+  describe 'checking the user.rb model' do
+    it 'should give the name of the user' do
+      user.name = 'Buff Daddy'
+    expect(user).to be_valid
+    end
+  end
+end
