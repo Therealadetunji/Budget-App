2ae85fc2f475908fc16fe5ef2e23fbe7c6490e45
Michael <madetunji1376@gmail.com>
2022-11-21 16:04:27 +0100

    models created from erb diagram

---
+++ app/models/group.rb
@@ -0,0 +1,11 @@
+class Group < ApplicationRecord
+    validates :name, presence: true
+    validates :icon, presence: true
+    validates :icon,
+    format: { with: /\.(gif|jpg|png|ico|jpeg)\Z/i,
+              message: 'must be a URL for GIF, JPG, PNG, ICO or Jpeg image.' }
+
+    belongs_to :author, class_name: 'User'
+    has_many :group_entities, dependent: :destroy
+    has_many :entities, through: :group_entities
+end
