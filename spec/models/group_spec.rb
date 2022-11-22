410adc7a0fc861c78d9f330e12615256e3896765
Michael <madetunji1376@gmail.com>
2022-11-22 22:35:13 +0100

    bootstrap added to the budget app for styling

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  
-    user = User.create(name: 'Buff Daddy')
-   group =  Group.new(author: user, name: 'Boys to men', icon: 'https://app_screenshot.png')
+  user = User.create(name: 'Buff Daddy')
+  group = Group.new(author: user, name: 'Boys to men', icon: 'https://app_screenshot.png')
 
   describe 'checking the user.rb model' do
-    
     it 'should give the name of the user' do
       group.name = 'Boys to men'
-    expect(group).to be_valid
+      expect(group).to be_valid
     end
 
     it 'should accept the png url' do
-    group.icon = 'app_screenshot.png'
-    expect(group).to be_valid
+      group.icon = 'app_screenshot.png'
+      expect(group).to be_valid
     end
   end
 end
