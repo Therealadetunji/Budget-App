38b19ef402060485418abbcbf99dbe3246a541e3
Michael <madetunji1376@gmail.com>
2022-11-24 16:36:56 +0100

    models adjusted when device was added

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -6,5 +6,7 @@ class Entity < ApplicationRecord
   has_many :group_entities, dependent: :destroy
   has_many :groups, through: :group_entities
 
-  def recent_transactions; end
+  def recent_transactions
+    entity.order(created_at: DESC).limit(3)
+ end
 end
