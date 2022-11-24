a284a6c4840bb9f26080893ee47f18440c7c4f7b
Michael <madetunji1376@gmail.com>
2022-11-24 10:26:34 +0100

    entity.rb model modified

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,7 +2,11 @@ class Entity < ApplicationRecord
   validates :name, presence: true
   validates :amount, presence: true
 
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :group_entities, dependent: :destroy
   has_many :groups, through: :group_entities
+
+  def recent_transactions
+    
+  end
 end
